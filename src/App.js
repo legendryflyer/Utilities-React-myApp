383a87e642d23a04003a7a29ad467b1f9b7d94f8
legendryflyer <tavish.anade@gmail.com>
2024-04-10 14:45:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Textform from './Camponants/Textform';
 function App() {
   return (
     <>
-      <div className="bg-secondary-subtle">
-        <Navbar title="textUtilities" aboutLinkText="About" />
-        <div className="container my-5">
-          <Textform heading="Enter the  text you want to analyze"/>
+      
+      <Navbar title="textUtilities" aboutLinkText="About" />
+      <div className="container my-5">
+        <Textform heading="Enter the  text you want to analyze"/>
         
-        </div>
       </div>
+      
     </>
 
   );
