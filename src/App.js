4688932bba0eb7df1236ccfeb5a63abe1a671f99
legendryflyer <tavish.anade@gmail.com>
2024-04-12 15:21:03 +0530

    adeed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './Camponants/About';
 import Navbar from './Camponants/Navbar';
 import Textform from './Camponants/Textform';
+import React, {useState} from 'react'
+
 
 
 function App() {
+  const [mode, setmode] = useState('light')
+  const toggleSwitch= ()=>{
+    if(mode==="light"){
+      setmode("dark")
+      document.body.style.backgroundColor='#1f1f1f'
+    }
+    else{
+      setmode("light")
+      document.body.style.backgroundColor='#fff'
+    }
+  }
   return (
     <>
       
-      <Navbar title="textUtilities" aboutLinkText="About" />
+      <Navbar title="textUtilities" aboutLinkText="About" mode={mode} toggleSwitch={toggleSwitch} />
+      {/* <About /> */}
       <div className="container my-5">
-        <Textform heading="Enter the  text you want to analyze"/>
-        
+        <Textform heading="Enter the  text you want to analyze" mode={mode}/>
       </div>
       
     </>
--- src/Camponants/Textform.js
+++ src/Camponants/Textform.js
@@ -52,6 +52,11 @@ export default function Textform(props) {
         document.body.removeChild(tempTextarea)
         
     }
+    const removeExtraSpaces = () => {
+        console.log("Remove extra spaces")
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(' '))
+    }
 
 
 
@@ -62,11 +67,11 @@ export default function Textform(props) {
 
     return (
         <>
-            <div>
+            <div className='container' style={{backgroundColor:props.mode==='light'?'white':'#1f1f1f', color:props.mode==='dark'?'white':'black'}}>
 
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="TextBox" rows="5"></textarea>
+                    <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='dark'?'#212529':'white', color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange}  id="TextBox" rows="5"></textarea>
                 </div>
                 <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                     <button type='button' className="btn btn-success" onClick={handleUpperOnClick}>Convert to Uppercase</button>
@@ -75,10 +80,11 @@ export default function Textform(props) {
                     <button type='button' className="btn btn-success" onClick={capitalizeCase}>Capitalize Case</button>
                     <button type='button' className="btn btn-success" onClick={alternateCase}>aLtErNaTiNg CaSe</button>
                     <button type='button' className="btn btn-success" onClick={copyToClipBoard}>Copy to Clipboard</button>
+                    <button type='button' className="btn btn-success" onClick={removeExtraSpaces}>Remove extra spaces</button>
                     <button type='button' className="btn btn-success" onClick={clearText}>Clear</button>
                 </div>
             </div>
-            <div>
+            <div className='container' style={{backgroundColor:props.mode==='light'?'white':'#1f1f1f', color:props.mode==='dark'?'white':'black'}}>
                 <h2 className="container my-3">Text Summary</h2>
                 <p className="container my-4">
                     {text.split(' ').length} words and {text.length} characters.
@@ -87,7 +93,7 @@ export default function Textform(props) {
                     You will require {0.008 * text.split(' ').length} minutes to read this in real life.
                 </p>
                 <h3 className="container my-3">Original Text Preview:</h3>
-                <p className="container my-4">{text}</p>
+                <p className="container my-4">{text.length>0?text:"No text entered"}</p>
 
 
             </div>
